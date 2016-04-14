7d0355d68f228967568e3e3a233ba422d385096d
Raphael Lacerda <lacerdaph@gmail.com>
2016-04-14 15:57:54 -0300

    colocando os projetos

---
+++ kumuluz/src/main/java/br/com/bb/RestResource.java
@@ -0,0 +1,30 @@
+package br.com.bb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/teste")
+public class RestResource {
+
+	@PersistenceContext(unitName = "jpa")
+	private EntityManager	em;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getResources() {
+
+		Map<String, String> json = new HashMap<>();
+		json.put("framework", "KumuluzEE");
+
+		System.out.println(em);
+		return Response.ok(json).build();
+	}
+}
---
+++ spark/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello;
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
